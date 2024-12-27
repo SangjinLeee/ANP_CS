bdff08c55a3feec0d4bf1f6883de6cf1d8450df0
Sangjin Lee <122560041+SangjinLeee@users.noreply.github.com>
2024-12-27 11:17:04 -0500

    a

--- cs/tailwind.config.js
+++ cs/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}", // Next.js 13+ App Directory 지원
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
